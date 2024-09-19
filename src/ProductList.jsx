c30f406a0344323d936658a8ff6ffc5de6eec14a
Laetitia Huriaux <laetitia.huriaux@gmail.com>
2024-09-19 11:27:40 -0400

    Improve design

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -285,8 +285,10 @@ const handlePlantsClick = (e) => {
                     <div className="product-list">
                         {category.plants.map((plant, plantIndex) => (
                         <div className="product-card" key={plantIndex}>
-                            <img className="product-image" src={plant.image} alt={plant.name} />
                             <div className="product-title">{plant.name}</div>
+                            <img className="product-image" src={plant.image} alt={plant.name} />
+                            <div className="product-price">{plant.cost}$</div>
+                            <div className="product-description">{plant.description}</div>
                             <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                         </div>
                         ))}
